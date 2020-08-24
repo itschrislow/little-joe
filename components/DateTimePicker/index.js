cfe741e23f328c2434b3c8c3d6c1a16ca27de17d
Chris Low <chrislow360@gmail.com>
2020-08-24 18:49:26 +0800

    capture date time component data

--- components/DateTimePicker/index.js
+++ components/DateTimePicker/index.js
@@ -2,14 +2,19 @@ import { useState } from 'react';
 import DateTimePicker from 'react-datetime-picker/dist/entry.nostyle';
 import styles from "./styles";
 
-const DateTimeComponent = () => {
+const DateTimeComponent = props => {
   const [datetime, setDatetime] = useState(new Date());
+
+  const handleChange = (newDatetime) => {
+    setDatetime(newDatetime)
+    props.handleChange(newDatetime);
+  }
 
   return (
     <>
       <DateTimePicker
         value={datetime}
-        onChange={setDatetime}
+        onChange={handleChange}
         maxDetail="minute"
         disableClock={true}
       />
