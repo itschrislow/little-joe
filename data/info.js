2c20f1cd7a5d7130a53d9499f39e7214864c1b75
Chris Low <chrislow360@gmail.com>
2020-08-27 01:58:09 +0800

    updated data

--- data/info.js
+++ data/info.js
@@ -1,6 +1,6 @@
 export default {
   title: "Little Joe",
   logo: "logo.png",
-  phone_number: "60123456789", // enter your own number
+  phone_number: "60105663227", // enter your own number
   about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut molestie libero. Pellentesque non varius nisi. Pellentesque eu justo a lectus volutpat bibendum. In luctus sapien id lectus accumsan, et pretium est accumsan. Praesent rutrum dui quis porta faucibus. Proin euismod orci urna, at lobortis nulla rhoncus sed. Integer congue interdum ipsum nec pharetra. Ut auctor quam urna, vel blandit velit dapibus quis."
 }
--- data/menu.js
+++ data/menu.js
@@ -1,22 +1,40 @@
 export default {
   items: [
     {
-      name: "Chicken Puff",
+      name: "Fudgy Oreo Brownies",
+      price: 35,
+      img: "brownies.jpeg",
+      desc: "Perfectly crisp top, super fudgy center and loaded with oreo crumbs.",
+    },
+    {
+      name: "Orange Chiffon Cake",
       price: 20,
-      img: "puff.jpeg",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut molestie libero.",
+      img: "orange.jpeg",
+      desc: "Additional RM3 for orange whipped cream topping.",
     },
     {
-      name: "Oreo Brownies",
-      price: 35,
-      img: "brownies.jpeg",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut molestie libero.",
+      name: "Earl Grey Chiffon Cake",
+      price: 20,
+      img: "earlgrey.jpeg",
+      desc: "Additional RM3 for salted cream topping.",
     },
     {
-      name: "Citrus Orange Cake",
-      price: 35,
-      img: "brownies.jpeg",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut molestie libero.",
+      name: "Coconut Pandan Chiffon Cake",
+      price: 15,
+      img: "coconut.jpeg",
+      desc: "Additional RM3 for salted cream topping.",
+    },
+    {
+      name: "Chocolate Chip Cookies ",
+      price: 10,
+      img: "cookie.jpeg",
+      desc: "Additional RM3 for salted cream topping.",
+    },
+    {
+      name: "Red Velvet Cupcake",
+      price: 6,
+      img: "cupcake.jpeg",
+      desc: "Additional RM 1 for gold flake.",
     }
   ]
 }
