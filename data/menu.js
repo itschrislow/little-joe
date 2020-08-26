6942597f3af6809532ab61a42a2d1ddfa9e36be8
Chris Low <chrislow360@gmail.com>
2020-08-26 19:41:29 +0800

    sample info and menu data

--- data/menu.js
+++ data/menu.js
@@ -2,15 +2,21 @@ export default {
   items: [
     {
       name: "Chicken Puff",
-      price: "20.00",
+      price: 20,
       img: "puff.jpeg",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut molestie libero.",
     },
     {
       name: "Oreo Brownies",
-      price: "35.00",
+      price: 35,
       img: "brownies.jpeg",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut molestie libero.",
     },
+    {
+      name: "Citrus Orange Cake",
+      price: 35,
+      img: "brownies.jpeg",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut molestie libero.",
+    }
   ]
 }
