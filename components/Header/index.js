6ef3c8dafd442b50ba50cddcdb4551030adf3d44
Chris Low <chrislow360@gmail.com>
2020-08-27 01:53:51 +0800

    add header links and styling

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,36 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NextLink from 'next/link';
 import data from "../../data/info";
+import { useRouter } from "next/router";
 import { Stack, Flex, Box, Divider, Text, Avatar } from "@chakra-ui/core";
+import styles from "./styles";
 
 const HeaderComponent = () => {
+  const router = useRouter();
+  useEffect(() => {
+    console.log(router.pathname)
+  }, [router.pathname]);
+
   return (
     <Box w="100%">
       <Flex flexDirection="column" align="center">
-        <Stack isInline align="center">
-          <Avatar name={data.title} src={data.logo}></Avatar>
-          <Text fontSize="4xl">{data.title}</Text>
-        </Stack>
+        <NextLink href="/menu" passHref>
+          <Stack isInline align="center" style={{ cursor: "pointer" }}>
+            <Avatar name={data.title} src={data.logo}></Avatar>
+            <Text fontSize="4xl">{data.title}</Text>
+          </Stack>
+        </NextLink>
       </Flex>
       <Divider />
       <Flex justify="center">
         <NextLink href="/about" passHref>
-          <Text pl="0.5rem" pr="0.5rem">
+          <Text
+            className={router.pathname === "/about" ? "active" : ""}
+            pl="0.5rem"
+            pr="0.5rem"
+            style={{ cursor: "pointer" }}
+          >
             About Us
           </Text>
         </NextLink>
         <NextLink href="/menu" passHref>
-          <Text pl="0.5rem" pr="0.5rem">
+          <Text
+            className={router.pathname === "/menu" ? "active" : ""}
+            pl="0.5rem"
+            pr="0.5rem"
+            style={{ cursor: "pointer" }}
+          >
             Menu
           </Text>
         </NextLink>
         <NextLink href="/order" passHref>
-          <Text pl="0.5rem" pr="0.5rem">
+          <Text
+            className={router.pathname === "/order" ? "active" : ""}
+            pl="0.5rem"
+            pr="0.5rem"
+            style={{ cursor: "pointer" }}
+          >
             Order Now
           </Text>
         </NextLink>
       </Flex>
       <Divider />
+      <style jsx>{styles}</style>
     </Box>
   );
 };
