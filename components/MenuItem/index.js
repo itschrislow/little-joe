da2ec167f6ad0f2ebe2490db205f1f500126203d
Chris Low <chrislow360@gmail.com>
2020-08-27 01:55:29 +0800

    redirect to order page when click menu items

--- components/MenuItem/index.js
+++ components/MenuItem/index.js
@@ -1,10 +1,18 @@
+import { useRouter } from 'next/router';
 import { Box, Image, Text } from "@chakra-ui/core";
 
 const MenuItemComponent = props => {
   const { name, price, img, desc } = props;
+  const router = useRouter();
 
   return (
-    <Box maxW="sm" overflow="hidden" rounded="lg" boxShadow="lg">
+    <Box
+      onClick={() => router.push("/order")}
+      maxW="sm"
+      overflow="hidden"
+      rounded="lg"
+      boxShadow="lg"
+    >
       <Image size="sm" height="300px" objectFit="cover" src={img} alt={name} />
       <Box p="10px">
         <Text style={{ float: "right" }}>RM {price.toFixed(2)}</Text>
