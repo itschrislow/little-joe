de36a608d6137b44cae5f350ef4abffc92d2a014
Chris Low <chrislow360@gmail.com>
2020-09-08 11:42:45 +0800

    updated info

--- data/info.js
+++ data/info.js
@@ -2,5 +2,6 @@ export default {
   title: "Little Joe",
   logo: "logo.png",
   phone_number: "60105663227", // enter your own number
-  about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut molestie libero. Pellentesque non varius nisi. Pellentesque eu justo a lectus volutpat bibendum. In luctus sapien id lectus accumsan, et pretium est accumsan. Praesent rutrum dui quis porta faucibus. Proin euismod orci urna, at lobortis nulla rhoncus sed. Integer congue interdum ipsum nec pharetra. Ut auctor quam urna, vel blandit velit dapibus quis."
+  about: "Little-joe is founded by 4 baking enthusiasts. We supply you with freshly baked cakes and pastries for your daily dose of happiness. Our baking goods are made with 100% premium ingredients. We will be donating 60% of our monthly profits to orphanages, old folks home and orang asli.",
+  mission: "To create a positive and loving environment where the less privileged feels at home and cared for by our society."
 }
