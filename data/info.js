bdb3d07b009d17ed44558c36b565ec0978993884
Chris Low <21194612+chris0610@users.noreply.github.com>
2020-11-02 21:18:35 +0800

    Update info.js

--- data/info.js
+++ data/info.js
@@ -1,7 +1,7 @@
 export default {
   title: "Little Joe",
   logo: "logo.png",
-  phone_number: "60105663227", // enter your own number
+  phone_number: "", // enter business or personal phone number
   about: "Little-joe is founded by 4 baking enthusiasts. We supply you with freshly baked cakes and pastries for your daily dose of happiness. Our baking goods are made with 100% premium ingredients. We will be donating 60% of our monthly profits to orphanages, old folks home and orang asli.",
   mission: "To create a positive and loving environment where the less privileged feels at home and cared for by our society."
 }
